94cf7f470ae56230334f5770c94608569c44bc12
waxew <4622.ar@gmail.com>
2026-08-29 02:22:48 +0330

    Route advanced QR and v1.2 measurement tools

--- app/src/main/java/com/asteam/toolbox/ui/screens/ToolRouter.kt
+++ app/src/main/java/com/asteam/toolbox/ui/screens/ToolRouter.kt
@@ -4,20 +4,25 @@ import androidx.compose.runtime.Composable
 import com.asteam.toolbox.data.ToolCategory
 import com.asteam.toolbox.data.ToolItem
 import com.asteam.toolbox.data.UserPreferences
+import com.asteam.toolbox.tools.AdvancedQrScreen
 import com.asteam.toolbox.tools.CalculationToolScreen
 import com.asteam.toolbox.tools.CameraToolScreen
 import com.asteam.toolbox.tools.ConverterToolScreen
 import com.asteam.toolbox.tools.EnhancedScannerToolScreen
 import com.asteam.toolbox.tools.EverydayToolScreen
 import com.asteam.toolbox.tools.MeasurementToolScreen
+import com.asteam.toolbox.tools.ProfessionalMeasurementToolScreen
 import com.asteam.toolbox.tools.SystemToolScreen
 
 @Composable
 fun ToolRouter(tool: ToolItem, preferences: UserPreferences) {
     when (tool.id) {
+        "qr" -> AdvancedQrScreen()
         "qr_scanner" -> EnhancedScannerToolScreen(onlyQr = true, preferences = preferences)
         "barcode_scanner" -> EnhancedScannerToolScreen(onlyQr = false, preferences = preferences)
         "scan_history", "magnifier", "mirror" -> CameraToolScreen(tool.id, tool.title, preferences)
+        "ruler", "protractor", "angle_meter", "vibrometer", "gps_dashboard", "distance_tracker", "sound_meter" ->
+            ProfessionalMeasurementToolScreen(tool.id, tool.title)
 
         else -> when (tool.category) {
             ToolCategory.MEASUREMENT -> MeasurementToolScreen(tool.id, tool.title)
