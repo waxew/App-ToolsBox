b77fe70f5c6923129723fe89f835040337c8f371
waxew <4622.ar@gmail.com>
2026-08-29 00:47:07 +0330

    Route v1.1.0 camera tools

--- app/src/main/java/com/asteam/toolbox/ui/screens/ToolRouter.kt
+++ app/src/main/java/com/asteam/toolbox/ui/screens/ToolRouter.kt
@@ -5,6 +5,7 @@ import com.asteam.toolbox.data.ToolCategory
 import com.asteam.toolbox.data.ToolItem
 import com.asteam.toolbox.data.UserPreferences
 import com.asteam.toolbox.tools.CalculationToolScreen
+import com.asteam.toolbox.tools.CameraToolScreen
 import com.asteam.toolbox.tools.ConverterToolScreen
 import com.asteam.toolbox.tools.EverydayToolScreen
 import com.asteam.toolbox.tools.MeasurementToolScreen
@@ -12,11 +13,16 @@ import com.asteam.toolbox.tools.SystemToolScreen
 
 @Composable
 fun ToolRouter(tool: ToolItem, preferences: UserPreferences) {
-    when (tool.category) {
-        ToolCategory.MEASUREMENT -> MeasurementToolScreen(tool.id, tool.title)
-        ToolCategory.CALCULATION -> CalculationToolScreen(tool.id, tool.title)
-        ToolCategory.CONVERSION -> ConverterToolScreen(tool.id, tool.title)
-        ToolCategory.TIME_DATE, ToolCategory.DIGITAL -> EverydayToolScreen(tool.id, tool.title, preferences)
-        ToolCategory.SYSTEM -> SystemToolScreen(tool.id, tool.title)
+    when (tool.id) {
+        "qr_scanner", "barcode_scanner", "scan_history", "magnifier", "mirror" ->
+            CameraToolScreen(tool.id, tool.title, preferences)
+
+        else -> when (tool.category) {
+            ToolCategory.MEASUREMENT -> MeasurementToolScreen(tool.id, tool.title)
+            ToolCategory.CALCULATION -> CalculationToolScreen(tool.id, tool.title)
+            ToolCategory.CONVERSION -> ConverterToolScreen(tool.id, tool.title)
+            ToolCategory.TIME_DATE, ToolCategory.DIGITAL -> EverydayToolScreen(tool.id, tool.title, preferences)
+            ToolCategory.SYSTEM -> SystemToolScreen(tool.id, tool.title)
+        }
     }
 }
