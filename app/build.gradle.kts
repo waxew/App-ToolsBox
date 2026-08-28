fb6d68f927f982fcae7a914f33cd79a15f59446b
waxew <4622.ar@gmail.com>
2026-08-29 02:45:30 +0330

    Promote App-ToolsBox to v2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.asteam.toolbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 11
+        versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
