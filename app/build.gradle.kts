bb71836b000192b8538d0d9f90efb684a459174e
waxew <4622.ar@gmail.com>
2026-08-29 12:11:14 +0330

    Bump App-ToolsBox to v2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.asteam.toolbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "2.0.0"
+        versionCode = 12
+        versionName = "2.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
